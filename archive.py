0fbd134cd372c2e8e8595dccd20670464fdc344d
Felipe Sulez <felipeantonio.sulez@gmail.com>
2023-09-10 23:46:28 -0500

    Creating highschool function

--- archive.py
+++ archive.py
@@ -9,10 +9,17 @@ a = input('\n Enter a name:')
 
 print(greetings(a))
 
+def highschool(name):
+    hs = f'The name of my highschool is:{name}'
+    return hs
 
+print(highschool(input('\n Enter the name of your highschool:')))
 
 def surname(surname_string):
     s = f'my surname is {surname_string}'
     n = f'{greetings(a)}'+ ' ' + s 
     return n
 print(surname(input('\n Enter your surname:')))
+
+
+

4726b12bf967ca16ae49ef6cbadcdb624376ea84
Felipe Sulez <felipeantonio.sulez@gmail.com>
2023-09-10 23:40:07 -0500

    Creating surname function

--- archive.py
+++ archive.py
@@ -1,6 +1,18 @@
+
+
 def greetings (prompt):
-    val = f'my namee is {prompt}'
+    val = f'my name is {prompt}'
 
     return val
 
-print(greetings(input('\n Enter a name:')))
+a = input('\n Enter a name:')
+
+print(greetings(a))
+
+
+
+def surname(surname_string):
+    s = f'my surname is {surname_string}'
+    n = f'{greetings(a)}'+ ' ' + s 
+    return n
+print(surname(input('\n Enter your surname:')))

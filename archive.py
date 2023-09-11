bfddef7cd0018a79f437a78d5a588c671e1a87e4
Felipe Sulez <felipeantonio.sulez@gmail.com>
2023-09-11 10:33:38 -0500

    Implement the rich library to manage the console

--- archive.py
+++ archive.py
@@ -1,12 +1,31 @@
+# importing libraries
+
+from rich import print
+from rich.console import Console
+
+
+
+console = Console()
+
+
 
 
 def greetings (prompt):
     val = f'my name is {prompt}'
 
+
     return val
 
-a = input('\n Enter a name:')
-b = input('\n Enter the name of your highschool:')
+def name():
+    a = input("\n Enter a name : ")
+
+    console.print(a,style='bold red')
+
+
+
+
+a = input("\n Enter a name : ")
+b = input('\n Enter the name of your highschool :')
 
 
 def highschool(name):
@@ -17,6 +36,8 @@ def main():
     def surname(surname_string):
         s = f'my surname is {surname_string}'
         n = f'{greetings(a)}'+ ' ' + 'and' + ' ' + s + ' ' 'and' + ' ' + f'{highschool(b)}' + '\n'
+        console.print('call the name function', style='bold magenta')
+        name()
         return n
     print(surname(input('\n Enter your surname:')))
 

8e63ad547b4e465770dc66affb56984429fd5040
Felipe Sulez <felipeantonio.sulez@gmail.com>
2023-09-10 23:54:53 -0500

    Join all functions surname, highschool and greet

--- archive.py
+++ archive.py
@@ -6,18 +6,17 @@ def greetings (prompt):
     return val
 
 a = input('\n Enter a name:')
+b = input('\n Enter the name of your highschool:')
 
-print(greetings(a))
 
 def highschool(name):
-    hs = f'The name of my highschool is:{name}'
+    hs = f'the name of my highschool is {name}'
     return hs
 
-print(highschool(input('\n Enter the name of your highschool:')))
 
 def surname(surname_string):
     s = f'my surname is {surname_string}'
-    n = f'{greetings(a)}'+ ' ' + s 
+    n = f'{greetings(a)}'+ ' ' + 'and' + ' ' + s + ' ' 'and' + ' ' + f'{highschool(b)}' + '\n'
     return n
 print(surname(input('\n Enter your surname:')))
 

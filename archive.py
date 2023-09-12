6b26f031fc3eb744897bd77105708003680d11de
Felipe Sulez <felipeantonio.sulez@gmail.com>
2023-09-11 20:36:32 -0500

    Integration data try exeption input data

--- archive.py
+++ archive.py
@@ -4,6 +4,7 @@ import re
 
 console = Console()
 
+
 def contains_special_characters(string_data):
     pattern = r'[!@#$%^&*()_+{}\[\]:;<>,.?~\\|]'
     return re.search(pattern, string_data) is not None
@@ -16,19 +17,26 @@ def input_string(prompt):
     return user_input
 
 def name_input():
-    try: 
-        name = input_string("\nEnter your name: ")
-        school = input_string("Enter the name of your high school: ")
+    global flag
 
-        if contains_special_characters(name):
-            console.print(f'\nThere is at least one special character', style='bold red')
-        elif contains_special_characters(school):
-            console.print(f'\nThere is at least one special character in', style='bold red')
+    name = input_string("\nEnter your name: ")
+    school = input_string("Enter the name of your high school: ")
 
+    try:
+        value1 = int(name)
+        value2 = int(school)
+
+        console.print(f'Warning,{value1} and {value2} ' ' ' +
+                      'are integers',style='bold magenta')
+        flag = False
+        return value1, value2
+
+    except ValueError:
+        flag = True
         return name, school
+    
+    
 
-    except ValueError as e:
-        print(e)
 
 def high_school(name):
     return f"The name of my high school is {name}"
@@ -37,14 +45,18 @@ def main():
     def surname(surname_string):
         name, school = name_input()
         surname_text = f"My surname is {surname_string}"
-        full_text = f"{greetings(name)} and {surname_text} and {high_school(school)}\n"
-        console.print("Calling the name_input function", style="bold magenta")
-        return full_text
+
+        if flag:
+            full_text = f"{greetings(name)} and {surname_text} and {high_school(school)}\n"
+            console.print("Calling the name_input function", style="bold magenta")
+            return full_text
+        else:
+            console.print('This values are not strings', style='bold red')
+            return None
 
     print(surname(input("\nEnter your surname: ")))
 
 if __name__ == "__main__":
     main()
-
 
 
